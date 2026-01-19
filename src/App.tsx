beb5d9dc15b14f25306b29611efb993b60776a7e
Tailung2 <155643057+Tailung1@users.noreply.github.com>
2026-01-19 23:30:27 +0400

    Create slide-in demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import Test from "./test";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [show, setShow] = useState<boolean>(false);
 
   return (
-    <>
+    <header>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+        {" "}
+        <p onClick={() => setShow(true)} className='show-button'>
+          show it
         </p>
+        <div className='header-content'>
+          <p>this is header content</p>
+        </div>
+ 
+        <AnimatePresence>
+          {show && (
+            <motion.div
+              initial={{ x: "-100vw" }}
+              animate={{ x: 0 }}
+              transition={{ duration: 1 }}
+              exit={{ x: "-100vw" }}
+              className='overlay'
+            >
+              <div onClick={() => setShow(false)}>back</div>
+              <div>Laptops</div>
+              <div>Phones</div>
+              <div>TV</div>
+              <div>Care</div>
+              <div>Gaming</div>
+              <div>More</div>
+            </motion.div>
+          )}
+        </AnimatePresence>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <div>
+        <Test />
+      </div>
+    </header>
+  );
 }
 
-export default App
+export default App;
---
+++ src/test.tsx
@@ -0,0 +1,23 @@
+
+
+export default function Test() {
+  return (
+    <div>
+      {" "}
+      <div>test</div>
+      <div>test</div>
+      <div>test</div>
+      <div>test</div>
+      <div>test</div>
+      <div>test</div>
+      <div>test</div>
+      <div>test</div>
+      <div>test</div>
+      <div>test</div>
+      <div>test</div>
+      <div>test</div>
+      <div>test</div>
+      <div>test</div>
+    </div>
+  );
+}

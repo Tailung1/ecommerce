7b8390624cb4a55a2820c52de27fa5d03d695102
Tailung2 <155643057+Tailung1@users.noreply.github.com>
2026-01-26 00:45:04 +0400

    Configure client package scrips to reach vite

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  root: "client",
+  //   root: "client", // once needed ..... :(
   plugins: [react()],
 });

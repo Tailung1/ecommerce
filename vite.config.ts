a4e6918682c4f08056e1013e8f2a7f414b92e4f3
Tailung2 <155643057+Tailung1@users.noreply.github.com>
2026-01-27 02:24:50 +0400

    Fix invalid hook errors and resolve project structure issues

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-    // root: "client", // once needed ..... :(
+    root: "client", // once needed ..... :(
   plugins: [react()],
 });

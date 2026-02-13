c990520f25a41b6229849f408a1d663edc4725c0
Tailung2 <155643057+Tailung1@users.noreply.github.com>
2026-02-13 21:05:25 +0400

    exclude id from params

--- client/src/pages/searching/Product.tsx
+++ client/src/pages/searching/Product.tsx
@@ -3,10 +3,13 @@ import { useParams } from "react-router-dom";
 
 export default function Product() {
   const { slug } = useParams();
+
   const productTitle = slug
     ?.split("-")
+    .slice(0, -1)
     .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
     .join(" ");
+
   const [product, setProduct] = useState<productType | null>(null);
 
   useEffect(() => {
@@ -27,7 +30,7 @@ export default function Product() {
   }, []);
   return (
     <div className='flex flex-col flex-grow '>
-      <h1>{`Product name: ${product?.name}`}</h1>
+      <h1>{productTitle}</h1>
       <h1>{`Product price: ${product?.price}`}</h1>
     </div>
   );

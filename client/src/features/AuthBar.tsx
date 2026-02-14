1607f47cee7e1ed786bd4d9c67352e7d10f57f82
Tailung2 <155643057+Tailung1@users.noreply.github.com>
2026-02-14 22:18:32 +0400

    Add smooth transition between authentication and register backgrounds

--- client/src/features/AuthBar.tsx
+++ client/src/features/AuthBar.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 export default function AuthBar({
   isExiting,
   setIsExiting,
@@ -5,9 +7,10 @@ export default function AuthBar({
 }: {
   isExiting: boolean;
   setIsExiting: (value: boolean) => void;
-
   setShowAuthBar: (value: boolean) => void;
 }) {
+  const [active, setActive] = useState<string>("auth");
+
   const handleExit = () => {
     setIsExiting(true);
     setTimeout(() => {
@@ -16,21 +19,28 @@ export default function AuthBar({
     }, 500);
   };
   return (
-    <div>
-      <div className={`authBar ${isExiting && "exit"}`}>
-        <p onClick={handleExit} className='exit-btn'>
-          X
-        </p>
-        <div className='auth-options flex gap-2'>
-          <p className='bg-orange-500'>Auth with number</p>
-          <p className='bg-orange-500'>Auth with gmail</p>
+    <div className={`authBar ${isExiting && "exit"}`}>
+      <p onClick={handleExit} className='exit-btn'>
+        X
+      </p>
+      <div className='w-full'>
+        <div className='flex flex-col gap-2 w-full'>
+          <div className='login-options'>
+            <p onClick={() => setActive("auth")}>Authentication</p>
+            <p onClick={() => setActive("register")}>Register</p>
+          </div>
+          <hr className={`${active ==="auth"?"bg-auth":"bg-register"}`} />
         </div>
-        <input
-          className='px-3 max-w-[100px]'
-          placeholder='Enter phone number'
-        />
-        <button>Submit</button>
       </div>
+      <div className='auth-options flex gap-2'>
+        <p className='bg-orange-500'>Auth with number</p>
+        <p className='bg-orange-500'>Auth with gmail</p>
+      </div>
+      <input
+        className='px-3 max-w-[100px]'
+        placeholder='Enter phone number'
+      />
+      <button>Submit</button>
     </div>
   );
 }

59ffc8a123a5afeef7e40571010493ba43c8cb14
Tailung2 <155643057+Tailung1@users.noreply.github.com>
2026-03-16 19:22:50 +0400

    Create custom reducer hook to be able to export it and use in auth bar

---
+++ client/src/MyReducer.tsx
@@ -0,0 +1,67 @@
+import { useReducer } from "react";
+
+const initialState: StateTypes = {
+  active: "auth",
+  activeAuthOption: "number",
+  isChecked: false,
+  showCountryCodes: false,
+  currentCode: "995",
+  inputValues: { email: "", password: "", number: "" },
+  errors: { emailError: "", passwordError: "", numberError: "" },
+};
+
+const useAuthReducer = () => {
+  function reducer(state: StateTypes, action: ActionTypes) {
+    switch (action.type) {
+      case "SET_ACTIVE":
+        return { ...state, active: action.payload };
+      case "SET_AUTH_OPTION":
+        return { ...state, activeAuthOption: action.payload };
+      case "SET_CHECKED":
+        return { ...state, isChecked: action.payload };
+      case "SET_INPUT":
+        return {
+          ...state,
+          inputValues: {
+            ...state.inputValues,
+            [action.field]: action.value,
+          },
+          errors: { ...state.errors, [`${action.field}Error`]: "" }, // clear error on input
+        };
+      case "SET_ERROR":
+        return {
+          ...state,
+          errors: { ...state.errors, [action.field]: action.value },
+        };
+      case "RESET_FORM":
+        return {
+          ...state,
+          inputValues: { email: "", password: "", number: "" },
+          errors: {
+            emailError: "",
+            passwordError: "",
+            numberError: "",
+          },
+          showCountryCodes: false,
+        };
+      case "TOGGLE_COUNTRY_CODES":
+        return {
+          ...state,
+          showCountryCodes: !state.showCountryCodes,
+        };
+      case "SET_COUNTRY_CODE":
+        return {
+          ...state,
+          currentCode: action.payload,
+          showCountryCodes: false,
+        };
+      default:
+        return state;
+    }
+  }
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+  return { state, dispatch };
+};
+
+export default useAuthReducer;
--- client/types.d.ts
+++ client/types.d.ts
@@ -10,3 +10,35 @@ type ProductType = {
   name: string;
   brand: string;
 };
+
+type StateTypes = {
+  active: "auth" | "register";
+  activeAuthOption: "email" | "number";
+  isChecked: boolean;
+  showCountryCodes: boolean;
+  currentCode: string;
+  inputValues: { email: string; password: string; number: string };
+  errors: {
+    emailError: string;
+    passwordError: string;
+    numberError: string;
+  };
+};
+
+type ActionTypes =
+  | { type: "SET_ACTIVE"; payload: "auth" | "register" }
+  | { type: "SET_AUTH_OPTION"; payload: "email" | "number" }
+  | { type: "SET_CHECKED"; payload: boolean }
+  | {
+      type: "SET_INPUT";
+      field: "email" | "password" | "number";
+      value: string;
+    }
+  | {
+      type: "SET_ERROR";
+      field: "emailError" | "passwordError" | "numberError";
+      value: string;
+    }
+  | { type: "RESET_FORM" }
+  | { type: "TOGGLE_COUNTRY_CODES" }
+  | { type: "SET_COUNTRY_CODE"; payload: string };

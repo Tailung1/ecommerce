7800177713afa86296e5763a37d23a46c71c67df
Tailung2 <155643057+Tailung1@users.noreply.github.com>
2026-03-27 13:04:29 +0400

    structure css files, create suitable folder for each graph

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import "./css/header-output.css";
-import "./css/main-output.css";
-import "./css/footer-output.css";
-import "./css/nav-output.css"
+import "./css/header/header-output.css";
+import "./css/main/main-output.css";
+import "./css/footer/footer-output.css";
+import "./css/btn-nav/nav-output.css";
+import "./css/compare@login-shared.scss";
 import App from "./App.tsx";
 import ContextProvider from "./MyContext";
 
--- client/src/pages/Main.tsx
+++ client/src/pages/Main.tsx
@@ -1,51 +1,28 @@
+import { useState } from "react";
 import { useMyContext } from "../MyContext";
 import SearchBar from "./searching/SearchBar";
 
 export default function Main() {
   const { showSearchBar } = useMyContext();
+
+  const [highlights, setHighlights] = useState([
+    { name: "iPhone 17 Pro | Pro Max" },
+    { name: "Computers" },
+    { name: "ONEPLUS 15R" },
+    { name: "Samsung 21s" },
+  ]);
+
   return (
     <div>
       {!showSearchBar ? (
         <div>
-          {" "}
-          <p className='w-full'>1</p>
-          <p className='w-full'>last</p>{" "}
-          <p className='w-full'>last</p>{" "}
-          <p className='w-full'>last</p>{" "}
-          <p className='w-full'>last</p>{" "}
-          <p className='w-full'>last</p>{" "}
-          <p className='w-full'>last</p>{" "}
-          <p className='w-full'>last</p>{" "}
-          <p className='w-full'>last</p>{" "}
-          <p className='w-full'>last</p>{" "}
-          <p className='w-full'>last</p>{" "}
-          <p className='w-full'>last</p>{" "}
-          <p className='w-full'>last</p>{" "}
-          <p className='w-full'>last</p>{" "}
-          <p className='w-full'>last</p>{" "}
-          <p className='w-full'>last</p>{" "}
-          <p className='w-full'>last</p>{" "}
-          <p className='w-full'>last</p>{" "}
-          <p className='w-full'>last</p>{" "}
-          <p className='w-full'>last</p>{" "}
-          <p className='w-full'>last</p>{" "}
-          <p className='w-full'>last</p>{" "}
-          <p className='w-full'>last</p>{" "}
-          <p className='w-full'>last</p>{" "}
-          <p className='w-full'>last</p>{" "}
-          <p className='w-full'>last</p>{" "}
-          <p className='w-full'>last</p>{" "}
-          <p className='w-full'>last</p>{" "}
-          <p className='w-full'>last</p>{" "}
-          <p className='w-full'>last</p>{" "}
-          <p className='w-full'>last</p>{" "}
-          <p className='w-full'>last</p>{" "}
-          <p className='w-full'>last</p>{" "}
-          <p className='w-full'>last</p>{" "}
-          <p className='w-full'>last</p>{" "}
-          <p className='w-full'>last</p>{" "}
-          <p className='w-full'>last</p>{" "}
-          <p className='w-full'>lastOne</p>
+          <section className='hightlights-wrapper'>
+            {highlights.map((h, index) => (
+              <div key={index}>
+                <p>{h.name}</p>
+              </div>
+            ))}
+          </section>
         </div>
       ) : (
         <SearchBar />

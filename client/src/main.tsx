bb2ba739e185c6b88f801b6aed7751aacda6e9f3
Tailung2 <155643057+Tailung1@users.noreply.github.com>
2026-04-17 06:49:48 +0400

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,15 +3,11 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import ContextProvider from "./MyContext";
-import { BarProvider } from "../contexts/BarProvider.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ContextProvider>
-      <BarProvider>
-        {" "}
-        <App />
-      </BarProvider>
+      <App />
     </ContextProvider>
   </StrictMode>
 );

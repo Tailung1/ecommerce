12b1ac97cc30d14dcac0c278b1546da04aca70f9
Tailung2 <155643057+Tailung1@users.noreply.github.com>
2026-06-07 18:35:22 +0400

    Create authMiddleware for token check

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,4 +1,5 @@
 import express from "express";
+import authMiddleware from "../server.js";
 import {
   register,
   login,
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import jwt from "jsonwebtoken";
 import productRouter from "./routes/productRoutes.js";
 import userRouter from "./routes/userRoutes.js";
 
@@ -14,6 +15,19 @@ app.use(
 );
 
 app.use(express.json());
+
+export default function authMiddleware(req, res, next) {
+  const token = req.cookies.token;
+  if (!token) return res.status(401).json({ message: "Not logged in" });
+
+  try {
+    const user = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = user;
+    next();
+  } catch (err) {
+    res.status(401).json({ message: "Invalid token" });
+  }
+}
 
 app.use("/api/products", productRouter);
 app.use("/api/users", userRouter);

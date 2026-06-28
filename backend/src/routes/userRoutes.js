8264f9997452aff83bf22d57986e7bbd491c3eed
Tailung2 <155643057+Tailung1@users.noreply.github.com>
2026-06-28 16:43:20 +0400

    Remove password reste related code from userRouter backend file

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,19 +1,9 @@
 import express from "express";
-import authMiddleware from "../server.js";
-import {
-  register,
-  login,
-  recoveryPassword,
-  requestPasswordReset,
-  verifyResetCode,
-} from "../controllers/userController.js";
+import { register, login } from "../controllers/userController.js";
 
 const userRouter = express.Router();
 
 userRouter.post("/login", login);
 userRouter.post("/register", register);
-userRouter.post("/requestPasswordReset", requestPasswordReset);
-userRouter.post("/verifyResetCode", verifyResetCode);
-userRouter.post("/recover-password", authMiddleware, recoveryPassword);
 
 export default userRouter;

ba8bb89243254f940850dc098b34c4e56da3575d
Tailung2 <155643057+Tailung1@users.noreply.github.com>
2026-07-14 02:32:50 +0400

    Fix vercel deploy error by fixed import issue in APp

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import router from "../routes";
+import router from "../routes"
 import { RouterProvider } from "react-router-dom";
 
 function App() {
--- client/src/Main.jsx
+++ client/src/Main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
+import App from "./App.jsx";
 import "./i18n.js";
 import ContextProvider from "./contexts/MyContext.tsx";
 import { BarProvider } from "./contexts/BarContext.tsx";

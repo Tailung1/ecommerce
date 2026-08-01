e1c121e00df10da38367062fbb6e4f629597e046
Tailung2 <155643057+Tailung1@users.noreply.github.com>
2026-08-01 23:53:28 +0400

    Update useWidth hook

--- client/src/contexts/MyContext.tsx
+++ client/src/contexts/MyContext.tsx
@@ -13,23 +13,11 @@ interface types {
   setPopularSearches: React.Dispatch<SetStateAction<productType[]>>;
   shoppingCart: any[];
   setShoppingCart: React.Dispatch<SetStateAction<productType[]>>;
-  resolution: { isIpad: boolean; isPc: boolean };
-  setResoltion: React.Dispatch<
-    SetStateAction<{
-      isIpad: boolean;
-      isPc: boolean;
-    }>
-  >;
 }
 
 const MyContext = createContext({} as types);
 
 export default function ContextProvider({ children }: { children: ReactNode }) {
-  const width = typeof window.innerWidth === "undefined" ? 0 : window.innerWidth;
-  const [resolution, setResoltion] = useState<{
-    isIpad: boolean;
-    isPc: boolean;
-  }>({ isIpad: width >= 768 && width < 1023, isPc: width > 1023 });
   const [activeCategory, setActiveCategory] = useState<string>("mobile-phones");
   const [popularSearches, setPopularSearches] = useState<productType[]>([]);
   const [shoppingCart, setShoppingCart] = useState<productType[] | []>([]);
@@ -43,8 +31,6 @@ export default function ContextProvider({ children }: { children: ReactNode }) {
         setPopularSearches,
         setShoppingCart,
         shoppingCart,
-        resolution,
-        setResoltion,
       }}
     >
       {children}
--- client/src/hooks/useWindowWidth.tsx
+++ client/src/hooks/useWindowWidth.tsx
@@ -1,38 +1,16 @@
-// import { useEffect, useState } from "react";
-
-// export default function useWindowWidth() {
-//   // typeof always returns a string, e.g., "object" for window or "undefined" if it doesn't exist
-//   const [width, setWidth] = useState(typeof window !== "undefined" ? window.innerWidth : 0);
-//   useEffect(() => {
-//     const handleResize = () => {
-//       setWidth(window.innerWidth);
-//     };
-//     window.addEventListener("resize", handleResize);
-
-//     return () => window.removeEventListener("resize", handleResize);
-//   }, []);
-
-//   return width;
-// }
-
-import { useEffect } from "react";
-import { useMyContext } from "../contexts/MyContext";
+import { useEffect, useState } from "react";
 
 export default function useWindowWidth() {
-  const { setResoltion } = useMyContext();
   // typeof always returns a string, e.g., "object" for window or "undefined" if it doesn't exist
-  //   const [width, setWidth] = useState(typeof window !== "undefined" ? window.innerWidth : 0);
+  const [width, setWidth] = useState(typeof window !== "undefined" ? window.innerWidth : 0);
   useEffect(() => {
     const handleResize = () => {
-      setResoltion({
-        isIpad: window.innerWidth >= 768 && window.innerWidth < 1024,
-        isPc: window.innerWidth > 1023,
-      });
+      setWidth(window.innerWidth);
     };
     window.addEventListener("resize", handleResize);
 
     return () => window.removeEventListener("resize", handleResize);
   }, []);
 
-  //   return width;
+  return width;
 }

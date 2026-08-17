07f461bd76fc4e0d6cbaf1242d2d3d0ce1a27312
Tailung2 <155643057+Tailung1@users.noreply.github.com>
2026-08-18 01:02:38 +0400

    Fix SearchBar import casing

--- client/routes.jsx
+++ client/routes.jsx
@@ -6,7 +6,7 @@ import Products from "./src/pages/main/Products/Products";
 import Cart from "./src/pages/main/ShoppingCart/ShoppingCart";
 import Compare from "./src/features/Compare/Compare";
 import Promotions from "./src/pages/main/Promotions/Promotions";
-import Search from "./src/pages/main/SearchBar/SearchBar";
+import SearchBar from "./src/pages/main/searchBar/SearchBar";
 import LanguageLayout from "./src/LanguageLayout";
 
 const shopRoutes = [
@@ -16,7 +16,7 @@ const shopRoutes = [
   },
   {
     path: "search",
-    element: <Search />,
+    element: <SearchBar />,
   },
   {
     path: "compare-products",

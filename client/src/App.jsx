24cc0629a80767d0bb7f13063081415f27a05374
Tailung2 <155643057+Tailung1@users.noreply.github.com>
2026-08-22 14:15:27 +0400

    Create tracker states

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,11 @@
-import router from "../routes"
+import router from "../routes";
 import { RouterProvider } from "react-router-dom";
+import CustomScrollbar from "./components/shared/CustomScrollbar/CustomScrollbar";
 
 function App() {
   return (
     <div className='flex flex-col min-h-screen'>
+      <CustomScrollbar />
       <RouterProvider router={router} />
     </div>
   );
--- client/src/components/shared/CustomScrollbar/CustomScrollbar.tsx
+++ client/src/components/shared/CustomScrollbar/CustomScrollbar.tsx
@@ -1,7 +1,31 @@
-import "./CustomScrollbar.scss"
+import "./CustomScrollbar.scss";
+import { useRef, useState, useEffect } from "react";
 
 export default function CustomScrollbar() {
+  const barRef = useRef<HTMLDivElement>(null);
+  const [isPressed, setIsPressed] = useState<boolean>(false);
+  const [isDragging, setIsDragging] = useState<boolean>(false);
+
+  const handlePointerDown = () => {
+    setIsPressed(true);
+  };
+  const handlePointerUp = () => {
+    setIsPressed(false);
+  };
+
+  const handleScroll = () => {
+    if (!isPressed) return;
+    setIsDragging(true);
+  };
+  useEffect(() => {}, [isDragging]);
+
   return (
-    <div>CustomScrollbar</div>
-  )
+    <div
+      onPointerDown={handlePointerDown}
+      onPointerMove={handleScroll}
+      onPointerUp={handlePointerUp}
+      ref={barRef}
+      className='customScrollbar-parent'
+    ></div>
+  );
 }

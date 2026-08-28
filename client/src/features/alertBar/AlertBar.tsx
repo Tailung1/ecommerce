bcf8f5cbb3a9f354b9ad3897500b8669bc93627a
Tailung2 <155643057+Tailung1@users.noreply.github.com>
2026-08-28 23:59:15 +0400

    combine bar context imports

--- client/src/features/alertBar/AlertBar.tsx
+++ client/src/features/alertBar/AlertBar.tsx
@@ -1,16 +1,16 @@
 import "./AlertBar.scss";
 import { useNavigate } from "react-router-dom";
 import exitBtn from "../../assets/reject.png";
-import rejectIcon from "../../assets/reject.png";
 import phoneImage from "../../assets/iphone.png";
-import { useBarDispatch } from "../../contexts/BarContext";
-import { useBarStateValue } from "../../contexts/BarContext";
+import { useBarDispatch, useBarStateValue } from "../../contexts/BarContext";
 import { useCompareCart } from "../../contexts/CompareContext";
 import ReactDOM from "react-dom";
 
 export default function AlertBar() {
   const navigate = useNavigate();
+
   const { setBar, setAlert } = useBarDispatch();
+
   const isCompareCartFull = useBarStateValue("alert").isFull;
   const isProductChosen = useBarStateValue("alert").isChosen;
   const isExitingBar = useBarStateValue("isExitingBar");
@@ -20,15 +20,11 @@ export default function AlertBar() {
   const handleReject = () => {
     setAlert("showAlert", false);
     navigate("/compare-products");
-    return;
   };
 
   return ReactDOM.createPortal(
     <div
       onAnimationEnd={(e) => {
-        // Checking isExitingBar is redundant in this case,
-        // but useful if there are multiple animations on this element.
-
         if (isExitingBar && e.animationName === "BarOut") {
           setAlert("showAlert", false);
           setBar("isExitingBar", false);
@@ -42,22 +38,26 @@ export default function AlertBar() {
         className='exit-btn exit-btn-warning w-10 h-8'
         alt='Exit icon'
       />
+
       <div className='warning-container'>
         <h2>{isProductChosen ? "Warning!" : "Compare"}</h2>
         <hr />
       </div>
+
       {isProductChosen ? (
         <p className='warning-reason-text'>Product is already chosen</p>
       ) : (
-        <div className='flex flex-col  items-center  gap-5 w-full'>
+        <div className='flex flex-col items-center gap-5 w-full'>
           <p className='text-red-600 text-[22px]'>
             {isCompareCartFull ? "You already have 4 products" : "Product adding is impossible"}
           </p>
+
           <p className='text-gray-500 text-[20px] text-center'>
             {isCompareCartFull
               ? "To add a new product, or remove one of them"
               : "Please choose another product from a different category or remove it"}
           </p>
+
           <section className='compare-products-parent'>
             {compareCart
               .filter((item) => item !== null)
@@ -67,15 +67,16 @@ export default function AlertBar() {
                     <img src={phoneImage} alt='phone image' />
                     <p>{prod?.name}</p>
                   </div>
-                  <img onClick={handleReject} src={rejectIcon} alt='Reject icon' />
+
+                  <img onClick={handleReject} src={exitBtn} alt='Reject icon' />
                 </div>
               ))}
-          </section>{" "}
+          </section>
         </div>
       )}
 
       <button onClick={() => setBar("isExitingBar", true)}>It is clear</button>
     </div>,
-    document && document.body
+    document.body
   );
 }

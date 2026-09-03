56d85cf65ff47508f6acf102afa01b5b5d5d9903
Tailung2 <155643057+Tailung1@users.noreply.github.com>
2026-09-03 23:59:24 +0400

    refactor: move compare products outside component

--- client/src/features/compare/CompareBar.tsx
+++ client/src/features/compare/CompareBar.tsx
@@ -1,49 +1,54 @@
 import searchIcon from "../../assets/search-icon.png";
-import { useBarDispatch } from "../../contexts/BarContext";
-import { useBarStateValue } from "../../contexts/BarContext";
+import { useBarDispatch, useBarStateValue } from "../../contexts/BarContext";
 import { useCompareDispatch } from "../../contexts/CompareContext";
+
+const testProducts = [
+  {
+    id: 1,
+    stock: 23,
+    price: 331,
+    name: "Laptop X",
+    brand: "TechCorp",
+    category: "phone",
+  },
+  {
+    id: 2,
+    name: "samsung tv-hd",
+    price: 312,
+    brand: "GizmoWorks",
+    stock: 2,
+    category: "tv",
+  },
+  {
+    id: 3,
+    name: "lenovo laptop",
+    stock: 5,
+    price: 392,
+    brand: "SoundMax",
+    category: "laptop",
+  },
+  {
+    id: 4,
+    price: 25,
+    stock: 21,
+    name: "Smartwatch A1",
+    brand: "TimeTech",
+    category: "phone",
+  },
+  {
+    id: 5,
+    stock: 3,
+    price: 451,
+    name: "Tablet Q",
+    brand: "ScreenMakers",
+    category: "tablet",
+  },
+];
 
 export default function CompareBar() {
   const { setBar } = useBarDispatch();
   const isExitingBar = useBarStateValue("isExitingBar");
   const { addCompareProduct } = useCompareDispatch();
-
-  const testProducts = [
-    { id: 1, stock: 23, price: 331, name: "Laptop X", brand: "TechCorp", category: "phone" },
-    {
-      id: 2,
-      name: "samsung tv-hd",
-      price: 312,
-      brand: "GizmoWorks",
-      stock: 2,
-      category: "tv",
-    },
-
-    {
-      id: 3,
-      name: "lenovo laptop",
-      stock: 5,
-      price: 392,
-      brand: "SoundMax",
-      category: "laptop",
-    },
-    {
-      id: 4,
-      price: 25,
-      stock: 21,
-      name: "Smartwatch A1",
-      brand: "TimeTech",
-      category: "phone",
-    },
-    {
-      id: 5,
-      stock: 3,
-      price: 451,
-      name: "Tablet Q",
-      brand: "ScreenMakers",
-      category: "tablet",
-    },
-  ];
 
   const handleAnimationEnd = (e: React.AnimationEvent<HTMLDivElement>) => {
     if (isExitingBar && e.animationName === "BarOut") {
@@ -54,12 +59,13 @@ export default function CompareBar() {
 
   return (
     <div
-      onAnimationEnd={(e) => handleAnimationEnd(e)}
+      onAnimationEnd={handleAnimationEnd}
       className={`Bar bar-modifed-compare ${isExitingBar ? "ExitBar" : ""}`}
     >
       <p onClick={() => setBar("isExitingBar", true)} className='exit-btn exit-btn-compare'>
         X
       </p>
+
       <div className='relative w-full'>
         <img className='absolute top-4 left-4' src={searchIcon} alt='Search Icon' />
 
@@ -69,16 +75,17 @@ export default function CompareBar() {
           type='text'
         />
       </div>
+
       {testProducts.map((prod) => (
         <div
           key={prod.id}
           onClick={() => {
             addCompareProduct(prod);
-            setBar("isExitingBar",true)
+            setBar("isExitingBar", true);
           }}
         >
-          {" "}
-          <h1>name:{prod.name}</h1> <h3>brand:{prod.brand}</h3>{" "}
+          <h1>name: {prod.name}</h1>
+          <h3>brand: {prod.brand}</h3>
         </div>
       ))}
     </div>

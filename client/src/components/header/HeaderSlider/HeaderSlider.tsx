dc0f6edc522900dc3566f77a3d0d5de5f7675be7
Tailung2 <155643057+Tailung1@users.noreply.github.com>
2026-09-05 16:22:21 +0400

    refactor: improve HeaderSlider structure and accessibility

--- client/src/components/header/HeaderSlider/HeaderSlider.tsx
+++ client/src/components/header/HeaderSlider/HeaderSlider.tsx
@@ -1,9 +1,9 @@
 import "./HeaderSlider.scss";
 import { easeOut, motion } from "framer-motion";
+import { useNavigate } from "react-router-dom";
+// import CategoryList from "../../../features/categories/components/shared/CategoryList";
 // import CategoryNavigation from "../../../features/categories/components/CategoryNavigation";
 import Categories from "../../reusable/Categories/Categories";
-// import CategoryList from "../../../features/categories/components/shared/CategoryList";
-import { useNavigate } from "react-router-dom";
 import BrandList from "./BrandList/BrandList";
 import searchIcon from "../../../assets/search-icon.png";
 import rejectIcon from "../../../assets/reject.png";
@@ -12,6 +12,15 @@ import { useBarDispatch } from "../../../contexts/BarContext";
 export default function HeaderSlider() {
   const navigate = useNavigate();
   const { setBar } = useBarDispatch();
+
+  const closeSidebar = () => {
+    setBar("showSideBar", false);
+  };
+
+  const handleSearchClick = () => {
+    navigate("/search");
+    closeSidebar();
+  };
 
   return (
     <motion.div
@@ -22,27 +31,18 @@ export default function HeaderSlider() {
       className='header-slider-container'
     >
       <div className='header-slider-exit-input-container'>
-        <img
-          src={rejectIcon}
-          className='cursor-pointer'
-          onClick={() => setBar("showSideBar", false)}
-        />
+        <img src={rejectIcon} alt='Close' className='cursor-pointer' onClick={closeSidebar} />
 
-        <div
-          onClick={() => {
-            navigate("/search");
-            setBar("showSideBar", false);
-          }}
-          className='serach-input-container'
-        >
+        <div onClick={handleSearchClick} className='search-input-container'>
           <div className='input-container'>
             <input className='input' placeholder='Search' type='text' />
-            <img className='search-icon' src={searchIcon} alt='search icon' />
+            <img className='search-icon' src={searchIcon} alt='Search' />
           </div>
         </div>
       </div>
+
       <div className='categories-and-brands-container'>
-        <Categories/>
+        <Categories />
         <BrandList />
       </div>
     </motion.div>

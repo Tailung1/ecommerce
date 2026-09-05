72fcb7a8c45110d9dd2612af8bc4a12e1780ab01
Tailung2 <155643057+Tailung1@users.noreply.github.com>
2026-09-05 16:12:16 +0400

    feat: add responsive header structure

--- client/src/components/header/Header.tsx
+++ client/src/components/header/Header.tsx
@@ -7,44 +7,14 @@ import cartIcon from "../../assets/shopping-cart.png";
 import loginIcon from "../../assets/login.png";
 import { useNavigate } from "react-router-dom";
 import Logo from "../shared/Logo/Logo";
-import SearchBar from "../../pages/main/searchBar/SearchBar";
-import { useEffect, useRef } from "react";
-import { useBarDispatch } from "../../contexts/BarContext";
-import { useBarStateValue } from "../../contexts/BarContext";
-import { useTranslation } from "react-i18next";
 
 export default function Header() {
-  const { setBar } = useBarDispatch();
-  const showSearchBar = useBarStateValue("showSearchBar");
-
-  const headerRef = useRef<HTMLElement | null>(null);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const Y = window.scrollY;
-      const header = headerRef.current;
-
-      if (!header) return;
-
-      if (Y > 127) {
-        header.classList.add("header-sticky");
-      } else if (Y < 57) {
-        header.classList.remove("header-sticky");
-      }
-    };
-
-    window.addEventListener("scroll", handleScroll);
-
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
-
-  const { t } = useTranslation();
   const navigate = useNavigate();
 
   return (
-    <header ref={headerRef}>
+    <header>
       <div className='header'>
-        <img onClick={() => setBar("showSideBar", true)} src={slideIcon} alt='Open menu' />
+        <img src={slideIcon} alt='Open menu' />
         <Logo />
       </div>
 
@@ -62,27 +32,22 @@ export default function Header() {
         <Logo />
 
         <div className='header-right-side-items'>
-          <button className='orange-btn'>{t("navigation")}</button>
+          <button className='orange-btn'>Navigation</button>
 
-          {showSearchBar ? (
-            <SearchBar />
-          ) : (
-            <div onClick={() => setBar("showSearchBar", true)} className='input-container'>
-              <input className='input' placeholder='Search' type='text' />
-              <img className='search-icon' src={searchIcon} alt='Search icon' />
-            </div>
-          )}
+          <div className='input-container'>
+            <input className='input' placeholder='Search' type='text' />
+            <img className='search-icon' src={searchIcon} alt='Search icon' />
+          </div>
 
           <div className='cart-container-header'>
             <img src={cartIcon} alt='Shopping cart icon' />
-            <span>{t("cart")}</span>
+            <span>Cart</span>
             <div className='invisible-div'></div>
-            <ShoppingCartPop />
           </div>
 
-          <div onClick={() => setBar("showAuthBar", true)} className='login-container-header'>
+          <div className='login-container-header'>
             <img src={loginIcon} alt='Login icon' />
-            <span>{t("login")}</span>
+            <span>Login</span>
           </div>
 
           <button className='orange-btn'>%</button>
--- client/src/features/compare/CompareBar.tsx
+++ client/src/features/compare/CompareBar.tsx
@@ -1,54 +1,49 @@
 import searchIcon from "../../assets/search-icon.png";
-import { useBarDispatch, useBarStateValue } from "../../contexts/BarContext";
+import { useBarDispatch } from "../../contexts/BarContext";
+import { useBarStateValue } from "../../contexts/BarContext";
 import { useCompareDispatch } from "../../contexts/CompareContext";
-
-const testProducts = [
-  {
-    id: 1,
-    stock: 23,
-    price: 331,
-    name: "Laptop X",
-    brand: "TechCorp",
-    category: "phone",
-  },
-  {
-    id: 2,
-    name: "samsung tv-hd",
-    price: 312,
-    brand: "GizmoWorks",
-    stock: 2,
-    category: "tv",
-  },
-  {
-    id: 3,
-    name: "lenovo laptop",
-    stock: 5,
-    price: 392,
-    brand: "SoundMax",
-    category: "laptop",
-  },
-  {
-    id: 4,
-    price: 25,
-    stock: 21,
-    name: "Smartwatch A1",
-    brand: "TimeTech",
-    category: "phone",
-  },
-  {
-    id: 5,
-    stock: 3,
-    price: 451,
-    name: "Tablet Q",
-    brand: "ScreenMakers",
-    category: "tablet",
-  },
-];
 
 export default function CompareBar() {
   const { setBar } = useBarDispatch();
   const isExitingBar = useBarStateValue("isExitingBar");
   const { addCompareProduct } = useCompareDispatch();
+
+  const testProducts = [
+    { id: 1, stock: 23, price: 331, name: "Laptop X", brand: "TechCorp", category: "phone" },
+    {
+      id: 2,
+      name: "samsung tv-hd",
+      price: 312,
+      brand: "GizmoWorks",
+      stock: 2,
+      category: "tv",
+    },
+
+    {
+      id: 3,
+      name: "lenovo laptop",
+      stock: 5,
+      price: 392,
+      brand: "SoundMax",
+      category: "laptop",
+    },
+    {
+      id: 4,
+      price: 25,
+      stock: 21,
+      name: "Smartwatch A1",
+      brand: "TimeTech",
+      category: "phone",
+    },
+    {
+      id: 5,
+      stock: 3,
+      price: 451,
+      name: "Tablet Q",
+      brand: "ScreenMakers",
+      category: "tablet",
+    },
+  ];
 
   const handleAnimationEnd = (e: React.AnimationEvent<HTMLDivElement>) => {
     if (isExitingBar && e.animationName === "BarOut") {
@@ -59,13 +54,12 @@ export default function CompareBar() {
 
   return (
     <div
-      onAnimationEnd={handleAnimationEnd}
+      onAnimationEnd={(e) => handleAnimationEnd(e)}
       className={`Bar bar-modifed-compare ${isExitingBar ? "ExitBar" : ""}`}
     >
       <p onClick={() => setBar("isExitingBar", true)} className='exit-btn exit-btn-compare'>
         X
       </p>
-
       <div className='relative w-full'>
         <img className='absolute top-4 left-4' src={searchIcon} alt='Search Icon' />
 
@@ -75,20 +69,18 @@ export default function CompareBar() {
           type='text'
         />
       </div>
-
       {testProducts.map((prod) => (
         <div
           key={prod.id}
           onClick={() => {
             addCompareProduct(prod);
-            setBar("isExitingBar", true);
+            setBar("isExitingBar",true)
           }}
         >
-          <h1>name: {prod.name}</h1>
-          <h3>brand: {prod.brand}</h3>
+          {" "}
+          <h1>name:{prod.name}</h1> <h3>brand:{prod.brand}</h3>{" "}
         </div>
       ))}
     </div>
   );
 }
-

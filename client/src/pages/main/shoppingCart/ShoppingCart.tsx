9640d7a7c50a2243d71a85d914f0303358e93d55
Tailung2 <155643057+Tailung1@users.noreply.github.com>
2026-09-10 20:41:36 +0400

    fix: use stable keys for cart items

--- client/src/pages/main/shoppingCart/ShoppingCart.tsx
+++ client/src/pages/main/shoppingCart/ShoppingCart.tsx
@@ -1,6 +1,12 @@
 import { useMyContext } from "../../../contexts/MyContext";
 import cartImage from "../../../assets/cartImage.png";
 
+interface CartItem {
+  id: string | number;
+  name: string;
+  price: number;
+}
+
 export default function Cart() {
   const { shoppingCart } = useMyContext();
 
@@ -10,7 +16,7 @@ export default function Cart() {
         <img className='w-[200px] h-[200px]' src={cartImage} alt='Shopping cart icon' />
       ) : (
         <div>
-          {shoppingCart.map((item: any) => (
+          {shoppingCart.map((item: CartItem) => (
             <div key={item.id} className='flex flex-col gap-3'>
               <h1>{item.name}</h1>
               <h2>{item.price}</h2>
